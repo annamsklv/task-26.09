a43972e0e9aaecf06d2280bb2d179a72474044dc
anna.msklv <anna.msklv@yandex.ru>
2022-09-30 22:38:16 +0300

    task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int sum = 0;
+
+for (int i = 0; i < number.Length; i++)
+{
+    if (int.TryParse(number[i].ToString(), out int result));
+    sum = sum + result;    
+}
+
+Console.WriteLine($"Сумма цифр в числе: {sum}");
