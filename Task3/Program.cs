abf3b7a438dcda83a3f1afea57de508c7567c60a
anna.msklv <anna.msklv@yandex.ru>
2022-09-30 23:03:08 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// ввод элеменетов через консоль (через запятую. использовать метод string.Split())
+
+
+string[] PrintArray(string arrayStr)
+{
+    string [] array = arrayStr.Split(", ");
+    return array;
+}
+
+Console.WriteLine("Введите элемены массива через запятую");
+string arrayStr = Console.ReadLine()??"";
+int arrayLenght = arrayStr.Length;
+string [] array = PrintArray(arrayStr);
+for (var i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]},");
+}
+
